2a95ad45a12daf072faee90c9dce88c53f0b69f1
VadimKalayda <vadosgta1@gmail.com>
2020-04-03 21:54:53 +0300

    Flex Component some features

--- src/components/FlexComponent/FlexComponent.tsx
+++ src/components/FlexComponent/FlexComponent.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderComponent from "../HeaderComponent/HeaderComponent";
 import { FlexImg, VideoBlock } from "./elements";
 import ReactPlayer from 'react-player'
 
 const FlexComponent = () => {
+  const [ showGiphNumber, setShowGiphNumber ] = useState(1);
+
+  setInterval(() => {
+    if( showGiphNumber < 3 ) {
+      setShowGiphNumber(showGiphNumber + 1);
+    } else {
+      setShowGiphNumber(1);
+    }
+  }, 10000);
+
   return(
     <>
       <HeaderComponent pageNameProps={'flex'} />
-      <FlexImg src="https://i.giphy.com/media/13FzDAoxlBesgM/giphy.webp" alt="Kek dancer"/>
+      { showGiphNumber === 1 ? <FlexImg src="https://i.giphy.com/media/13FzDAoxlBesgM/giphy.webp" alt="Kek dancer"/> : null }
+      { showGiphNumber === 2 ? <FlexImg src="https://i.giphy.com/media/Jb56O0QSZRopG/giphy.webp" alt="Kek dancer"/> : null }
+      { showGiphNumber === 3 ? <FlexImg src="https://media3.giphy.com/media/3zSaOGAwLHhxC/giphy.gif?cid=ecf05e47f64cc31222f0ca3df1b875665f8ace4335fcc56f&rid=giphy.gif" alt="Kek dancer"/> : null }
       <VideoBlock style={{display: 'none'}}>
         <ReactPlayer url={'https://soundcloud.com/mikkel-roed-767734565/1-hour-of-shreksophone'} volume={0.1} playing={true} loop={true} />
       </VideoBlock>
